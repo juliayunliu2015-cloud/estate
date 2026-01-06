795b6ee17f529a7f872e0e46213a1a470abe2a54
juliayunliu2015-cloud <juliayunliu2015@gmail.com>
2026-01-06 18:14:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def create_dashboard(df, comparison_cols):
             cell.set_facecolor('white' if row % 2 == 0 else '#F2EFED')
 
     ax_table.set_title(f"Market Report: {comparison_cols[0]} vs {comparison_cols[1]}", 
-                       fontsize=30, fontweight='bold', pad=40)
+                       fontsize=30, fontweight='bold', pad=40, y=1.1)
 
     # --- CHART SECTION ---
     ax_chart = fig.add_subplot(gs[1])
